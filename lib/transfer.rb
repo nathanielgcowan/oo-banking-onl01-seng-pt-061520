09fbafd2a289de80404a4a339cd22c3a9c68e290
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-25 22:41:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+  attr_reader 
+  attr_accessor :balance, :status
+  
+  def initialize(transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+  
+  def valid?
+    balance > 0 && status== "open"
+  end
+  
+  def execute_transaction
+    self.status = "closed"
+  end
+  
+  def reverse_transfer
+    
+  end
+  
 end
