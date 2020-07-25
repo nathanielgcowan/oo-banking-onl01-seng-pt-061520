36b26f3f2eda83ba5bf9abca024e5c72c393eced
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-25 22:59:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   
   def execute_transaction
     if valid? && sender.balance > amount && self.status = "pending"
-      sender.balnce -= amount
+      sender.balance -= amount
       receiver.balance +=amount
       self.status == "complete"
     else
