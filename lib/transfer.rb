d20fe4882a586012adfcbbb1641cef71eef35b16
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-25 23:03:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
   end
   
   def reverse_transfer
-      if valid? && sender.balance > amount && self.status = "complete"
+      if valid? && receiver.balance > amount && self.status = "complete"
       sender.balance += amount
       receiver.balance -=amount
       self.status = "reversed"
