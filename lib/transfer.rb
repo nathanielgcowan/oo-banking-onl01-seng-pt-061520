c2c07bcd80e17235cbb8dfbd56caddf4b4661048
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-25 23:06:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status = "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance +=amount
       self.status = "complete"
@@ -24,7 +24,7 @@ class Transfer
   end
   
   def reverse_transfer
-      if valid? && receiver.balance > amount && self.status = "complete"
+      if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -=amount
       sender.balance += amount
       self.status = "reversed"
