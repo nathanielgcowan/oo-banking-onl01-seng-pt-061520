8bf3795b86aff7a3f4523240cd6b0d72ae3b4a0e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-25 22:24:22 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
-
+  attr_reader :name
+  
+  def initialize(name)
+    @name=name
+    @balance = 1000
+    status = 'open'
+  end
+  
+  def deposit
+    self << deposit
+  end
+  
+  def display_balance
+    display_balance
+  end
+  
+  def valid
+    if status = 'open' && balance > log10
+      valid
+    end
+  end
+  
+  def close_account
+    self << close
+  end
+  
 end
